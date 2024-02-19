83598c5e4525b4308b3aa9cbc7b7dbc05ee7af06
kvarzzDRZ <dmitryskvortcov2005@gmail.com>
2024-02-19 15:53:03 +0300

    Выполнил все задания

--- task5/main.js
+++ task5/main.js
@@ -1,6 +1,6 @@
 let banknote = {
     nominal: 100,
-    currency: '$',
+    currency: 'USD',
     issueCountry: "USA",
     releaseYear: "1999",
     serialNumber: 17862265
@@ -11,4 +11,20 @@ for(item in banknote)
     console.log(`${item}: ${banknote[item]}`);
 }
 
-
+console.log("\n");
+if (banknote["currency"] === "RUB")
+{
+    console.log("Валюта банкноты - рубли");
+}
+else if (banknote["currency"] === "EUR")
+{
+    console.log("Валюта банкноты - евро");
+}
+else if (banknote["currency"] === "JPY")
+{
+    console.log("Валюта банкноты - юани");
+}
+else if (banknote["currency"] === "USD")
+{
+    console.log("Валюта банкноты - доллары");
+}
